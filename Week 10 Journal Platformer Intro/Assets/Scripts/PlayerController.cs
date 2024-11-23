988089d4ec7e884d0ed39b16c1234e8bea916524
sin-sord <148483596+sin-sord@users.noreply.github.com>
2024-11-22 20:10:28 -0500

    Task 1 complete
    Player can move using Rigidbody2D

--- Week 10 Journal Platformer Intro/Assets/Scripts/PlayerController.cs
+++ Week 10 Journal Platformer Intro/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,16 @@
+using System.Timers;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public float speed;
+    float moveHorizontal;
+
+
+
+
     public enum FacingDirection
     {
         left, right
@@ -10,7 +19,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,12 +28,14 @@ public class PlayerController : MonoBehaviour
         // The input from the player needs to be determined and
         // then passed in the to the MovementUpdate which should
         // manage the actual movement of the character.
-        Vector2 playerInput = new Vector2();
+        Vector2 playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
         MovementUpdate(playerInput);
+
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
+        rb.AddForce(playerInput * speed * Time.deltaTime);
 
     }
 
